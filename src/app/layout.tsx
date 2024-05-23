4be230213dc83e11b24f4e58a0286c80eb60c31e
Cefas Cavalcanti <cavalcanticefas@gmail.com>
2024-05-23 19:47:58 -0300

    openGraph edit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
   title: "Convite",
   description: "Você é nosso convidado especial",
   openGraph: {
-    images: 'https://lh7-us.googleusercontent.com/NFJpDrHNQnxETZmWAKwWQKJaySf_y1G1eeZX1H5sO7o9Vz7NBscQ77bLKKdNyZFNmqzLxy60s414LgbFuli7xrkQYlD3wclQhAQi2KfpXsq6OLvXoOcxKQl6geediDs80HWU_VIHNmj-4GYu',
+    images: 'https://lh7-us.googleusercontent.com/NHApaQZJNtHf-BVaBBrW36-_UJJx9Z1J164ajLBNjUOOe9aKLu8yC2QI80Sr4c29BmKktYapMu7-FiWw2FOAnDi27N921z9ndXKg6-EIlsM0AEUUHDEp8KtmraB5q0IvH_26OLyLALFQV0Jq',
   },
   keywords: ["Convites"],
 };
