2a812be121a4b98e4d3793066836c83ccd6b3384
Cefas Cavalcanti <cavalcanticefas@gmail.com>
2024-09-11 13:13:36 -0300

    convite implementação

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,9 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Convite",
   description: "Você é nosso convidado especial",
-  openGraph: {
-    images: 'https://lh7-us.googleusercontent.com/NHApaQZJNtHf-BVaBBrW36-_UJJx9Z1J164ajLBNjUOOe9aKLu8yC2QI80Sr4c29BmKktYapMu7-FiWw2FOAnDi27N921z9ndXKg6-EIlsM0AEUUHDEp8KtmraB5q0IvH_26OLyLALFQV0Jq',
-  },
+
   keywords: ["Convites"],
 };
 
