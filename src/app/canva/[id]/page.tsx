6157c805bc7feada92d75f16b57cce0b8001cbef
Cefas Cavalcanti <cavalcanticefas@gmail.com>
2024-09-17 10:46:41 -0300

    canva apdate button

--- src/app/canva/[id]/page.tsx
+++ src/app/canva/[id]/page.tsx
@@ -78,7 +78,7 @@ export default async function Form({ params, searchParams }: Props) {
         <div className="flex h-full p-1 w-full gap-4 items-center justify-center">
           <Link href={`${product.local}`}>
             <button className={`px-1 overflow-auto bg-green-800  items-center justify-center flex font-semibold rounded-md text-white`} type="button" >
-              < LottiePlayer className="h-10 gap-4 -ml-1" src="https://lottie.host/e7ed314d-d186-48fc-8ea1-bd75f74388d4/WbfHdjYY09.json" />
+              < LottiePlayer className="h-10 -mx-4" src="https://lottie.host/e7ed314d-d186-48fc-8ea1-bd75f74388d4/WbfHdjYY09.json" />
               Localização
             </button>
           </Link>
